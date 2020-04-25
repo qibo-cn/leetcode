37ac6f7be132be9c904ad6227e499649c7bf11d0
Qibo <qibo_cn@outlook.com>
2020-04-25 20:17:37 +0800

    leetcode project init

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1;
+    int b = 2;
+    int c = 0;
+
+    c = a + b;
+    
+    printf("hello\n");
+printf("hello\n");
+printf("hello\n");
+printf("hello\n");
+    return 1;
+}
